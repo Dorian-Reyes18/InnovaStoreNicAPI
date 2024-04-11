8c9a3ec24a4b5c6f2ab6087d99afe97293c7dedd
Dorian Reyes <doreyes@gbm.net>
2024-04-10 18:39:59 -0600

    Se agregan las variables de entorno y se completa la estructura del proyecto

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import rolesRoutes from "./routes/rolesRoutes.js";
+import productosRoutes from "./routes/productosRoutes.js";
+import infoClientesRoutes from "./routes/infoClientesRoutes.js";
+import usuariosRoutes from "./routes/usuariosRoutes.js";
+import login from "./routes/loginRoutes.js";
+import estadoEntrega from "./routes/estadoEntregaRoutes.js";
+import pagoPorVenta from "./routes/pagoPorVentaRoutes.js";
+import ordenesVenta from "./routes/ordenVentaRoute.js";
+import detalleOrdenVenta from "./routes/detalleOrdenVentaRoutes.js";
+import entrega from "./routes/entregaRoutes.js";
+import entregaFinalizada from "./routes/entregaFinalizadaRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", rolesRoutes);
+app.use("/api", productosRoutes);
+app.use("/api", infoClientesRoutes);
+app.use("/api", usuariosRoutes);
+app.use("/api", login);
+app.use("/api", estadoEntrega);
+app.use("/api", pagoPorVenta);
+app.use("/api", ordenesVenta);
+app.use("/api", detalleOrdenVenta);
+app.use("/api", entrega);
+app.use("/api", entregaFinalizada);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "enpoint Not found",
+  });
+});
+
+export default app;
